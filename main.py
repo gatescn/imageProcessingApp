1b67bbcdb10c3c8511a7f99974b1ecc3ae734734
Corey Gates <workofgates@Coreys-MacBook-Air.local>
2022-02-22 12:56:54 -0500

    added salt and pepper filter

--- main.py
+++ main.py
@@ -1,8 +1,47 @@
-#main script of image processing app
+# main script of image processing app
 import os
-from PIL import Image
+import random
+from PIL import Image, ImageFile
 
-#reads config file
+
+class SaltAndPepperFilter:
+
+    trashcan = []
+
+    def setstrength(self):
+        if self.strength == 1:
+            self.strength = (-1, 0, 0, 1)
+            return
+        if self.strength == 2:
+            self.strength = (-1, -1, 0, 1, 1)
+            return
+        self.strength = (-1,0,0,0,0,0,0,0,0,0,0,0 ,1)
+        return
+
+    def __init__(self, strength, matrix):
+        self.strength = strength
+        self.imgMatrix = matrix
+        self.setstrength()
+
+    def performfilter(self):
+        for r, rows in enumerate(self.imgMatrix):
+            for c, col in enumerate(rows):
+                pixelvalue = self.computevalue(col)
+                self.imgMatrix[r][c] = pixelvalue
+
+    def computevalue(self, currentpixel):
+        decider = random.randint(0, len(self.strength) - 1)
+        choice = self.strength[decider]
+        if choice == -1:
+            # salt
+            currentpixel = 0
+        if choice == 1:
+            # pepper
+            currentpixel = 255
+        return currentpixel
+
+
+# reads config file
 def read_config():
     filename = "external.config"
     contents = open(filename).read()
@@ -11,15 +50,36 @@ def read_config():
     processedimagedirectory = config['outputDirectory']
     return rawimagedirectory, processedimagedirectory
 
+
 if __name__ == '__main__':
     imageDictionary = {}
-    rawdirectory,outputdirectory = read_config()
+    ImageFile.LOAD_TRUNCATED_IMAGES = True
+    rawdirectory, outputdirectory = read_config()
     files = os.listdir(rawdirectory)
+    index = 0
     for file in files:
-        filepath = rawdirectory+"/"+file
+        index += 1
+        filepath = rawdirectory + "/" + file
+        # the raw image
         rawimg = Image.open(filepath)
-        newimg = rawimg
+        # copy of the raw image
+        newimgdata = []
+        # create copy of raw image data into editable lists.
         data = list(rawimg.getdata())
-        #operations here....
-        newimg.save(outputdirectory+"/"+file)
+        for t in data:
+            tempList = []
+            for i in t:
+                tempList.append(i)
+            newimgdata.append(tempList)
+        width = rawimg.width
+        height = rawimg.height
+        s = SaltAndPepperFilter(1, newimgdata)
+        s.performfilter()
+        newimgdatafinal = []
+        for s in newimgdata:
+            tuplelist = tuple(s)
+            newimgdatafinal.append(tuplelist)
+        newImg = Image.new(rawimg.mode, rawimg.size)
+        newImg.putdata(newimgdatafinal)
+        newImg.save(outputdirectory + "/" + file)
         break
